ded0482e8e9b1845796a49e8da34f080e8831e40
Anurag Patel <mail2anuragpatel@gmail.com>
2024-06-20 23:09:28 +0530

    circuit breaker implementation

--- src/main/java/com/anurag/foodcatalogue/service/FoodCatalogueService.java
+++ src/main/java/com/anurag/foodcatalogue/service/FoodCatalogueService.java
@@ -6,10 +6,12 @@ import com.anurag.foodcatalogue.dto.RestaurantDTO;
 import com.anurag.foodcatalogue.entity.FoodItem;
 import com.anurag.foodcatalogue.mapper.FoodItemMapper;
 import com.anurag.foodcatalogue.repository.FoodItemRepository;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
@@ -28,6 +30,7 @@ public class FoodCatalogueService {
         return FoodItemMapper.INSTANCE.mapFoodItemToFoodItemDTO(foodAdded);
     }
 
+    @CircuitBreaker(name = "restaurantMSBreaker", fallbackMethod = "restaurantMSFallback")
     public FoodCataloguePageDTO foodCataloguePageDetails(Integer restaurantId){
         //fetch food Item from this service
         List<FoodItem> foodItemList = fetchFoodItemList(restaurantId);
@@ -44,8 +47,25 @@ public class FoodCatalogueService {
         return foodItemRepository.findByRestaurantId(restaurantId);
     }
 
-    private RestaurantDTO fetchRestaurantDetailsFromRestaurantMS(Integer restaurantId) {
-        return restTemplate.getForObject("http://RESTAURANT-SERVICE/restaurant/fetchById/"+restaurantId, RestaurantDTO.class);
+    public RestaurantDTO fetchRestaurantDetailsFromRestaurantMS(Integer restaurantId) {
+        try {
+            return restTemplate.getForObject("http://RESTAURANT-SERVICE/restaurant/fetchById/" + restaurantId, RestaurantDTO.class);
+        }catch(RestClientException ex){
+            System.err.println("RestClientException caught: " + ex.getMessage());
+            throw new RuntimeException("Failed to fetch restaurant details", ex);
+        }
+    }
+
+    public FoodCataloguePageDTO restaurantMSFallback(Integer restaurantId, Throwable throwable){
+        FoodCataloguePageDTO foodCataloguePageDTO = new FoodCataloguePageDTO();
+        RestaurantDTO restaurantDTO = new RestaurantDTO();
+        restaurantDTO.setId(1);
+        restaurantDTO.setCity("not found");
+        restaurantDTO.setName("failure");
+        restaurantDTO.setAddress("404 - not found!!!");
+        foodCataloguePageDTO.setRestaurant(restaurantDTO);
+
+        return foodCataloguePageDTO;
     }
 
     private FoodCataloguePageDTO createFoodCataloguePage(List<FoodItem> foodItemList, RestaurantDTO restaurant) {
